e3b85072d5561bc12ffac2668b25b8320eab2cbb
igor-makariew <34421941+igor-makariew@users.noreply.github.com>
2021-12-20 12:23:50 +0300

    многопоточное скачивание

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -217,15 +217,130 @@ class SiteController extends Controller
                 'limit' => $data['filter']['limit']
             ]
         ];
-        // изменить на не последовательное а на синхронное!!!
-        foreach ($urlsSer as $index => $urlSer) {
-            $ch[$index] = curl_init();
-            curl_setopt($ch[$index], CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch[$index], CURLOPT_URL, $urlsSer[$index] . '?' . http_build_query($options[$index]));
-            $res[$index] = curl_exec($ch[$index]);
-            $response[$index] = json_decode($res[$index], true);
-            curl_close($ch[$index]);
+        // Скачивание сразу нескольких страниц
+        // инициализируем "контейнер" мультизапросов (мультикурл)
+        $multi_init = curl_multi_init();
+
+        // массив отдельных заданий
+        $jobs = [];
+
+        // проходим по каждому URL-адресу
+        foreach ($urlsSer as $index =>  $urlSer) {
+            // подключаем отдельный поток (URL-адрес)
+            $init = curl_init($urlSer. '?' . http_build_query($options[$index]));
+
+            // если произойдёт перенаправление, то перейти по нему
+            curl_setopt($init, CURLOPT_FOLLOWLOCATION, 1);
+
+            // curl_exec вернёт результат
+            curl_setopt($init, CURLOPT_RETURNTRANSFER, 1);
+
+            // таймаут соединения 10 секунд
+            curl_setopt($init, CURLOPT_CONNECTTIMEOUT, 10);
+
+            // таймаут ожидания также 10 секунд
+            curl_setopt($init, CURLOPT_TIMEOUT, 10);
+
+            // HTTP-заголовок ответа не будет возвращён
+            curl_setopt($init, CURLOPT_HEADER, 0);
+
+            // добавляем дескриптор потока в массив заданий
+            $jobs[$urlSer] = $init;
+
+            // добавляем дескриптор потока в мультикурл
+            curl_multi_add_handle($multi_init, $init);
         }
+
+        // кол-во активных потоков
+        $thread = null;
+
+        // запускаем исполнение потоков
+        do {
+            $thread_exec = curl_multi_exec($multi_init, $thread);
+        }
+        while ($thread_exec == CURLM_CALL_MULTI_PERFORM);
+
+        // исполняем, пока есть активные потоки
+        while ($thread && ($thread_exec == CURLM_OK)) {
+
+            // если поток готов к взаимодествию
+            if (curl_multi_select($multi_init) != -1) {
+
+                // ждем, пока что-нибудь изменится
+                do {
+                    $thread_exec = curl_multi_exec($multi_init, $thread);
+
+                    // читаем информацию о потоке
+                    $info = curl_multi_info_read($multi_init);
+
+                    // если поток завершился
+                    if ($info['msg'] == CURLMSG_DONE) {
+
+                        $init = $info['handle'];
+
+                        // ищем URL страницы по дескриптору потока в массиве заданий
+                        $page = array_search($init, $jobs);
+
+                        // скачиваем содержимое страницы
+                        $jobs[$page] = curl_multi_getcontent($init);
+
+                        // распарисиваем и сохряняем ее
+                        $response[] = json_decode($jobs[$page], true);
+
+                        // удаляем поток из мультикурла
+                        curl_multi_remove_handle($multi_init, $init);
+
+                        // закрываем отдельный поток
+                        curl_close($init);
+
+                    }
+                }
+                while ($thread_exec == CURLM_CALL_MULTI_PERFORM);
+            }
+        }
+        // закрываем мультикурл
+        curl_multi_close($multi_init);
+
+        // изменить на не последовательное а на синхронное!!!
+//        $mch = curl_multi_init();
+//        $chs = [];
+//        foreach ($urlsSer as $index => $urlSer) {
+//            $chs[$index] = ($ch = curl_init());
+//            curl_setopt($ch, CURLOPT_URL, $urlsSer[$index] . '?' . http_build_query($options[$index]));
+//            curl_setopt($ch, CURLOPT_HEADER, 0 );
+//            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//            curl_multi_add_handle($mch, $ch);
+//        }
+//
+//        $prev_running = $running = null;
+//        do {
+//            curl_multi_exec($mch, $running);
+//            if ($running != $prev_running) {
+//                $info = curl_multi_info_read($mch);
+//                if (is_array($info) && ($ch = $info['handle'])) {
+//                    $content = curl_multi_getcontent( $ch );
+//                    $response[] = json_decode($content, true);
+//                }
+//                $prev_running = $running;
+//            }
+//        } while ($running > 0);
+//
+//        foreach ( $chs as $ch ) {
+//            curl_multi_remove_handle( $mch, $ch );
+//            curl_close( $ch );
+//
+//        }
+//        curl_multi_close($mch);
+
+
+//        foreach ($urlsSer as $index => $urlSer) {
+//            $ch[$index] = curl_init();
+//            curl_setopt($ch[$index], CURLOPT_RETURNTRANSFER, true);
+//            curl_setopt($ch[$index], CURLOPT_URL, $urlsSer[$index] . '?' . http_build_query($options[$index]));
+//            $res[$index] = curl_exec($ch[$index]);
+//            $response[$index] = json_decode($res[$index], true);
+//            curl_close($ch[$index]);
+//        }
 
         $list = [];
         foreach ($response as $listHotels) {
